8e671a53cc3aa58010c410e7369011ca7fe18a2c
firasnajjar59 <firasnajjar92@gmail.com>
2022-11-25 13:16:37 +0200

    make it better

--- app.js
+++ app.js
@@ -8,17 +8,16 @@ const cardsRouter = require("./Routes/Cards/cardsRouter");
 const chalk = require("chalk");
 const morgan = require("morgan");
 const cors = require("cors");
-require("dotenv").config()
+require("dotenv").config();
 app.use(morgan(chalk.cyan(":method :url :status :response-time ms")));
 app.use(cors());
 app.use(express.json());
 app.use("/api/users", usersRouter);
 app.use("/api/cards", cardsRouter);
 router.get("/", async (req, res) => {
- 
-    return res.json({"messege":"hi"});
-})
-const PORT = 8181;
-app.listen(process.env.PORT||PORT, () =>
+  return res.json({ messege: "hi" });
+});
+const PORT = process.env.PORT || 8181;
+app.listen(PORT, () =>
   console.log(chalk.blueBright.bold(`server run on: http://:localhost:${PORT}`))
 );
