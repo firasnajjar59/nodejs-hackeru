9bb399dc8183907bc8f209a7a0c5769e493d9f42
firasnajjar59 <firasnajjar92@gmail.com>
2022-11-25 12:40:41 +0200

    make it better

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require("./DB/connectToDb");
 // require("./primeryData/primeryCards")();
 const express = require("express");
 const app = express();
-
+const router = express.Router();
 const usersRouter = require("./Routes/Users/userRouter");
 const cardsRouter = require("./Routes/Cards/cardsRouter");
 const chalk = require("chalk");
@@ -14,7 +14,10 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/users", usersRouter);
 app.use("/api/cards", cardsRouter);
-
+router.get("/", async (req, res) => {
+ 
+    return res.json({"messege":"hi"});
+})
 const PORT = 8181;
 app.listen(process.env.PORT||PORT, () =>
   console.log(chalk.blueBright.bold(`server run on: http://:localhost:${PORT}`))
