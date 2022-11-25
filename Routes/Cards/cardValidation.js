dd7a2ab75ce5809180a536c414786b3a96227d68
firasnajjar59 <firasnajjar92@gmail.com>
2022-11-25 17:34:19 +0200

    make it better

--- Routes/Cards/cardValidation.js
+++ Routes/Cards/cardValidation.js
@@ -1,4 +1,4 @@
-const Joi = require("Joi");
+const Joi = require("joi");
 
 function validateCard(card) {
   const schema = Joi.object({
--- Routes/Users/usersValidations/registraion.js
+++ Routes/Users/usersValidations/registraion.js
@@ -1,4 +1,4 @@
-const Joi = require("Joi");
+const Joi = require("joi");
 
 function validateRegistration(user) {
   const schema = Joi.object({
